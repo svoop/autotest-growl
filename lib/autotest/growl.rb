0a7f265d5849bc67c54b32ce04159ea89a094ed1
Sven Schwyn <schwyn@bitcetera.com>
2009-05-28 20:51:12 +0200

    Show cucumber notifications only if cucumber is used. Add remote_notification and hide_label configurations. Fix local icon sets. Update documentation.

--- lib/autotest/growl.rb
+++ lib/autotest/growl.rb
@@ -19,15 +19,28 @@ module Autotest::Growl
   VERSION  = '0.1.0'
   GEM_PATH = File.expand_path(File.join(File.dirname(__FILE__), '../..'))
 
-  REMOTE_NOTIFICATION = false
+  @@remote_notification = false
+  @@hide_label = false
+
+  ##
+  # Whether to use remote or local notificaton (default).
+  def self.remote_notification=(boolean)
+    @@remote_notification = boolean
+  end
+
+  ##
+  # Whether to display the label (default) or not.
+  def self.hide_label=(boolean)
+    @@hide_label = boolean
+  end
 
   ##
   # Display a message through Growl.
   def self.growl title, message, icon, priority=0, stick=""
     growl = File.join(GEM_PATH, 'growl', 'growlnotify')
-    image = File.join('~', '.autotest-growl', "#{icon}.png")
+    image = File.join(ENV['HOME'], '.autotest-growl', "#{icon}.png")
     image = File.join(GEM_PATH, 'img', "#{icon}.png") unless File.exists?(image)
-    if REMOTE_NOTIFICATION
+    if @@remote_notification
       system "#{growl} -H localhost -n autotest --image '#{image}' -p #{priority} -m #{message.inspect} '#{title}' #{stick}"
     else
       system "#{growl} -n autotest --image '#{image}' -p #{priority} -m #{message.inspect} '#{title}' #{stick}"
@@ -37,7 +50,7 @@ module Autotest::Growl
   ##
   # Set the label and clear the terminal.
   Autotest.add_hook :run_command do
-    @label = File.basename(Dir.pwd).upcase
+    @label = (@@hide_label) ? ('') : (File.basename(Dir.pwd).upcase + ': ')
     @run_scenarios = false
     print "\n"*2 + '-'*80 + "\n"*2
     print "\e[2J\e[f"   # clear the terminal
@@ -49,15 +62,15 @@ module Autotest::Growl
   Autotest.add_hook :ran_command do |autotest|
     gist = autotest.results.grep(/\d+\s+(example|test)s?/).map {|s| s.gsub(/(\e.*?m|\n)/, '') }.join(" / ")
     if gist == ''
-      growl "#{@label} cannot run tests", '', 'error'
+      growl @label + 'Cannot run tests.', '', 'error'
     else
       if gist =~ /[1-9]\d*\s+(failure|error)/
-        growl "#{@label} fails some tests", "#{gist}", 'failed'
+        growl @label + 'Some tests have failed.', gist, 'failed'
       elsif gist =~ /pending/
-        growl "#{@label} has pending tests", "#{gist}", 'pending'
+        growl @label + 'Some tests are pending.', gist, 'pending'
         @run_scenarios = true
       else
-        growl "#{@label} passes all tests", "#{gist}", 'passed'
+        growl @label + 'All tests have passed.', gist, 'passed'
         @run_scenarios = true
       end
     end
@@ -66,17 +79,17 @@ module Autotest::Growl
 
   # FIXME: This is a temporary workaround until Cucumber is properly integrated!
   Autotest.add_hook :waiting do |autotest|
-    if @run_scenarios
+    if @run_scenarios && autotest.results.grep(/cucumber/)
       gist = autotest.results.grep(/\d+\s+(scenario|step)s?/).map {|s| s.gsub(/(\e.*?m|\n)/, '') }.join(" / ")
       if gist == ''
-        growl "#{@label} cannot run scenarios", '', 'error'
+        growl @label + 'Cannot run scenarios.', '', 'error'
       else
         if gist =~ /failed/
-          growl "#{@label} fails some scenarios", "#{gist}", 'failed'
+          growl @label + 'Some scenarios have failed.', gist, 'failed'
         elsif gist =~ /undefined/
-          growl "#{@label} has undefined scenarios", "#{gist}", 'pending'
+          growl @label + 'Some scenarios are undefined.', gist, 'pending'
         else
-          growl "#{@label} passes all scenarios", "#{gist}", 'passed'
+          growl @label + 'All scenarios have passed.', gist, 'passed'
         end
       end
     end
