c547d23e423c84d61a3eef09d68304eca1387594
Sven Schwyn <schwyn@bitcetera.com>
2009-06-08 11:33:00 +0200

    Add some tests, however, still broken at @at.hook(:ran_command)

--- spec/autotest-growl_spec.rb
+++ spec/autotest-growl_spec.rb
@@ -1,1 +1,61 @@
 require File.dirname(__FILE__) + '/spec_helper.rb'
+
+describe "handling results" do
+  before do
+    @at = Autotest.new
+  end
+
+  describe "for RSpec" do
+    it "should show a passing growl" do
+      Autotest::Growl.should_receive(:growl).and_return('passed')
+      @at.results = ["10 examples, 0 failures"]
+      @at.hook(:ran_command)
+    end
+
+    it "should show a failing growl" do
+      Autotest::Growl.should_receive(:growl).and_return('passed')
+      @at.results = ["10 examples, 1 failures"]
+      @at.hook(:ran_command)
+    end
+
+    it "should show a pending growl" do
+      Autotest::Growl.should_receive(:growl).and_return('passed')
+      @at.results = ["10 examples, 0 failures, 1 pending"]
+      @at.hook(:ran_command)
+    end
+  end
+
+  describe "for Cucumber" do
+    # not sure what cucumber output looks like
+    # it "should show a passing growl" do
+    #   @at.results = ["10 examples, 0 failures"]
+    #   @at.hook(:ran_command)
+    # end
+    #
+    # it "should show a failing growl" do
+    #   @at.results = ["10 examples, 1 failures"]
+    #   @at.hook(:ran_command)
+    # end
+    #
+    # it "should show a pending growl" do
+    #   @at.results = ["10 examples, 0 failures, 1 pending"]
+    #   @at.hook(:ran_command)
+    # end
+  end
+
+  describe "for Test::Unit" do
+    it "should show a passing growl" do
+      Autotest::Growl.should_receive(:growl).and_return('passed')
+      @at.results = ["1 tests, 1 assertions, 0 failures, 0 errors"]
+      @at.hook(:ran_command)
+    end
+
+    it "should show a failing growl" do
+      Autotest::Growl.should_receive(:growl).twice.and_return('passed')
+      @at.results = ["1 tests, 1 assertions, 1 failures, 0 errors"]
+      @at.hook(:ran_command)
+      @at.results = ["1 tests, 1 assertions, 0 failures, 1 errors"]
+      @at.hook(:ran_command)
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,4 +7,10 @@ rescue LoadError
 end
 
 $:.unshift(File.dirname(__FILE__) + '/../lib')
-require 'autotest-growl'
+require 'autotest/growl'
+
+module Autotest::Growl
+  def self.growl(title, message, icon, priority=0, stick="")
+    icon
+  end
+end
