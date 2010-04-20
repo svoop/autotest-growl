68bc45c86388dbdb74af936641f53f6e7cccb9f3
Ben Ritcey <github@lists.ritcey.com>
2010-04-21 03:47:14 +0800

    added sticky_errors option

--- lib/autotest/growl.rb
+++ lib/autotest/growl.rb
@@ -28,6 +28,7 @@ module Autotest::Growl
   @@hide_label = false
   @@show_modified_files = false
   @@image_dir = File.join(GEM_PATH, 'img', 'ruby')
+  @@sticky_errors = false
 
   ##
   # Whether to use remote or local notificaton (default).
@@ -67,6 +68,12 @@ module Autotest::Growl
     else
       @@image_dir = path
     end
+  end
+
+  ##
+  # Whether to make error messages sticky.
+  def self.sticky_errors=(boolean)
+    @@sticky_errors = boolean
   end
 
   ##
@@ -117,18 +124,19 @@ module Autotest::Growl
     unless @@one_notification_per_run && @ran_tests
       result = Autotest::Result.new(autotest)
       if result.exists?
+        sticky_switch = @@sticky_errors ? '-s' : ''
         case result.framework
         when 'test-unit'        
           if result.has?('test-error')
-            growl @label + 'Cannot run some unit tests.', "#{result.get('test-error')} in #{result.get('test')}", 'error', 2
+            growl @label + 'Cannot run some unit tests.', "#{result.get('test-error')} in #{result.get('test')}", 'error', 2, sticky_switch
           elsif result.has?('test-failed')
-            growl @label + 'Some unit tests failed.', "#{result['test-failed']} of #{result.get('test-assertion')} in #{result.get('test')} failed", 'failed', 2
+            growl @label + 'Some unit tests failed.', "#{result['test-failed']} of #{result.get('test-assertion')} in #{result.get('test')} failed", 'failed', 2, sticky_switch
           else
             growl @label + 'All unit tests passed.', "#{result.get('test-assertion')} in #{result.get('test')}", 'passed', -2
           end
         when 'rspec'
           if result.has?('example-failed')
-            growl @label + 'Some RSpec examples failed.', "#{result['example-failed']} of #{result.get('example')} failed", 'failed', 2
+            growl @label + 'Some RSpec examples failed.', "#{result['example-failed']} of #{result.get('example')} failed", 'failed', 2, sticky_switch
           elsif result.has?('example-pending')
             growl @label + 'Some RSpec examples are pending.', "#{result['example-pending']} of #{result.get('example')} pending", 'pending', -1
           else
@@ -136,7 +144,7 @@ module Autotest::Growl
           end
         end
       else
-        growl @label + 'Could not run tests.', '', 'error', 2
+        growl @label + 'Could not run tests.', '', 'error', 2, sticky_switch
       end
       @ran_tests = true
     end
@@ -149,6 +157,7 @@ module Autotest::Growl
     unless @@one_notification_per_run && @ran_features
       result = Autotest::Result.new(autotest)
       if result.exists?
+        sticky_switch = @@sticky_errors ? '-s' : ''
         case result.framework
         when 'cucumber'
           explanation = []
@@ -159,7 +168,7 @@ module Autotest::Growl
           elsif result.has?('scenario-failed') || result.has?('step-failed')
             explanation << "#{result['scenario-failed']} of #{result.get('scenario')} failed" if result['scenario-failed']
             explanation << "#{result['step-failed']} of #{result.get('step')} failed" if result['step-failed']
-            growl @label + 'Some Cucumber scenarios failed.', "#{explanation.join("\n")}", 'failed', 2
+            growl @label + 'Some Cucumber scenarios failed.', "#{explanation.join("\n")}", 'failed', 2, sticky_switch
           elsif result.has?('scenario-pending') || result.has?('step-pending')
             explanation << "#{result['scenario-pending']} of #{result.get('scenario')} pending" if result['scenario-pending']
             explanation << "#{result['step-pending']} of #{result.get('step')} pending" if result['step-pending']
@@ -169,7 +178,7 @@ module Autotest::Growl
           end      
         end
       else
-        growl @label + 'Could not run features.', '', 'error', 2
+        growl @label + 'Could not run features.', '', 'error', 2, sticky_switch
       end
       @ran_features = true
     end
